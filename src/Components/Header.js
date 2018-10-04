c1c5d314e94760d35e81611884eb87d54df50ac7
RehaS <beratson@gmail.com>
2018-10-04 18:49:00 +0200

    navbar infographic link open in new tab cancelled

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -26,7 +26,7 @@ class Header extends Component {
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-	         <li><Link target='#'  to={"/infographic"}>Life</Link></li>
+	         <li><Link  to={"/infographic"}>Life</Link></li>
             {/* <li><a className="smoothscroll" href="#portfolio">Works</a></li> */}
             {/* <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li> */}
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
